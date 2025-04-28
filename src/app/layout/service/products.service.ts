ba284d1698144b32e68955fc144988c8c68b45be
Zer093 <105790577+Zer093@users.noreply.github.com>
2025-04-28 18:05:17 +0300

    Products

--- src/app/layout/service/products.service.ts
+++ src/app/layout/service/products.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from "@angular/core";
-import { ProductResponse } from "src/app/modules/products/products.module";
+import { LayoutService } from 'src/app/layout/service/layout.service';
+import { HttpClientService } from "src/app/Core/services/http-client.service";
+import { ProductResponse, ProductRequest, ProductSearchRequest, ProductUpdateRequest } from "src/app/modules/products/products.module";
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class ProductService{
-    public SelectedData: ProductResponse | null = null;
+  providedIn: 'root'
+})
+export class ProductService {
+  public SelectedData: ProductResponse | null = null;
+  public Dialog: any | null = null;
+  constructor(public layoutService: LayoutService, public httpClient: HttpClientService) { }
+  async Add(data: ProductRequest) {
+    const apiUrl = `/api/product`;
+
+    return await this.httpClient.post(apiUrl, data);
+  }
+
+  async Update(data: ProductUpdateRequest) {
+
+    const apiUrl = `/api/product`;
+    return await this.httpClient.put(apiUrl, data);
+  }
+
+  async Delete(uuid: string) {
+
+    const apiUrl = `/api/product/${uuid}`;
+    return await this.httpClient.delete(apiUrl, uuid)
 
   }
+
+  async Search(filter: ProductSearchRequest) {
+
+    const apiUrl = `/api/product/list?${this.layoutService.Filter(filter)}`;
+
+    return await this.httpClient.get(apiUrl)
+
+  }
+}
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
-import { AddProductComponent } from './add-product/add-product.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ResponseBase } from 'src/app/shared/class/class';
+import { ResponseBase, RequestBase, SearchRequestBase } from 'src/app/shared/class/class';
+import { AddProductComponent } from './add-product/add-product.component';
 
 
 @NgModule({
@@ -17,13 +16,44 @@ import { ResponseBase } from 'src/app/shared/class/class';
   imports: [
     CommonModule,
     ProductsRoutingModule,
-     SharedModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class ProductsModule { }
 export interface ProductResponse extends ResponseBase {
 
+  uuid?: string;
+  productTranslation?: { [key: string]: ProductTranslationResponse };
+  price: string,
 
+}
+export interface ProductSearchRequest extends SearchRequestBase {
+  uuid?: string;
+  name?: string;
 
+}
+export interface ProductUpdateRequest extends RequestBase {
+  productTranslation?: ProductTranslationRequest[];
+  price: string,
+}
+
+export interface ProductRequest extends RequestBase {
+  productTranslation?: ProductTranslationRequest[];
+  price: string,
+}
+
+export interface ProductTranslationResponse {
+  uuid?: string;
+  name?: string;
+  language?: string;
+}
+export interface ProductTranslationRequest {
+  uuid?: string;
+  name?: string;
+  language?: string;
+}
+export interface ProductTranslationUpdateRequest {
+  uuid?: string;
+  name?: string;
 }
