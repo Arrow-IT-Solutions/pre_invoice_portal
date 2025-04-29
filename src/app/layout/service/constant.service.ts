d7e9914030026683e9a784f4176f0eb064405907
Zer093 <105790577+Zer093@users.noreply.github.com>
2025-04-29 23:35:30 +0300

    Invoice

---
+++ src/app/layout/service/constant.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { LayoutService } from 'src/app/layout/service/layout.service';
+import { environment } from 'src/environments/environment';
+import { HttpClientService } from 'src/app/Core/services/http-client.service';
+import { UserService } from './user.service';
+import Axios from 'axios';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConstantService {
+  constructor(
+    public layoutService: LayoutService,
+    public userService: UserService,
+    public httpClient: HttpClientService
+  ) { }
+
+  // async Search(name: string) {
+  //   const config = {
+  //     headers: {
+  //       'Accept-Language': this.layoutService.config.lang,
+  //       Authorization: 'Bearer ' + this.userService.currentUser.token,
+  //     },
+  //   };
+
+  //   const apiUrl = `${this.layoutService.baseApiUrl
+  //     }/api/constants?${this.layoutService.Filter({ enumName: name })}`;
+
+  //   return Axios.get(apiUrl, config)
+  //     .then((response) => {
+  //       console.log('r', response);
+
+  //       return response.data;
+  //     })
+  //     .catch((error) => {
+  //       console.log('rr', error);
+  //       return error.response.data;
+  //     });
+  // }
+
+
+  async Search(name: string, isDefault: Number = 1) {
+
+    const apiUrl = `/api/constants?${this.layoutService.Filter({ enumName: name, isDefaultOptionAll: isDefault })}`;
+
+    return await this.httpClient.get(apiUrl)
+
+  }
+
+}
+export interface ConstantResponse {
+  key?: string;
+  value?: string;
+  color?: string;
+}
