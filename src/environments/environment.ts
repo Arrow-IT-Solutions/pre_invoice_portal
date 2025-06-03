aa7576be0ee579ffc60d107d5458dca51bf85322
Nour <noursaleem@arrowitjo.com>
2025-06-03 19:34:15 +0300

    fix questions for user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //baseApiUrl: "https://invoice-dev-api.apps-jo.com",
-  baseApiUrl: "https://localhost:49853"
+  baseApiUrl: "https://invoice-dev-api.apps-jo.com"
 
 };
 
